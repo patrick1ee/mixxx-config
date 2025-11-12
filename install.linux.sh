7c616456f08528aeb6f224b2b04f3d3b30f602b3
patrick <patrick@lodg.er>
2025-11-12 19:40:27 +0000

    remove install.linux

--- install.linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Installs all controller mappings and scripts to ~/.mixxx/controllers
-
-# Exit on first error
-set -e
-
-mkdir -p ~/.mixxx/controllers
-cp ./controllers/* ~/.mixxx/controllers/
